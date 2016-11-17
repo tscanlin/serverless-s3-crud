efb7e439d4855ae40f54e6afa6f9e720b383b387
Tim Scanlin <tscanlin@gmail.com>
2016-11-17 00:53:28 -0800

    start getting s3 working

--- todos-read-all.js
+++ todos-read-all.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (event, callback) => {
@@ -8,10 +9,24 @@ module.exports = (event, callback) => {
     TableName: 'todos',
   };
 
-  return dynamoDb.scan(params, (error, data) => {
-    if (error) {
-      callback(error);
-    }
-    callback(error, data.Items);
-  });
+  const s3params = {
+    Bucket: 'todos',
+    Key: 'test1',
+    Body: JSON.stringify(params)
+  }
+
+  S3.createBucket(s3params, () => {
+    
+  })
+
+  return S3.upload(s3params, (err, data) => {
+    console.log(err, data);
+  })
+
+  // return dynamoDb.scan(params, (error, data) => {
+  //   if (error) {
+  //     callback(error);
+  //   }
+  //   callback(error, data.Items);
+  // });
 };
