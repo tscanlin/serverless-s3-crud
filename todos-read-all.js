86aa912fe90b2df4497434a78429ab1eb0b2eef1
Tim Scanlin <tscanlin@gmail.com>
2016-11-19 14:50:28 -0800

    get dynamo db working in addition to s3

--- todos-read-all.js
+++ todos-read-all.js
@@ -2,11 +2,16 @@
 
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient({
+  region: 'us-east-1'
+});
+
+// listBuckets, listObjectsV2
 
 module.exports = (event, callback) => {
   const params = {
     TableName: 'todos',
+    region: 'us-east-1'
   };
 
   const s3bucket = {
@@ -14,12 +19,12 @@ module.exports = (event, callback) => {
   }
   const s3params = {
     Bucket: 'todos-new',
-    Key: 'test1',
+    Key: 'a/test1',
     Body: 'something else!!!'
   }
   const s3GetParams = {
     Bucket: 'todos-new',
-    Key: 'test1',
+    Key: 'a/test1',
   }
   console.log('test');
 
@@ -27,21 +32,32 @@ module.exports = (event, callback) => {
   //   console.log(err, d);
   //   // console.log(d.Body.toString());
   // })
+  var start = +new Date();
 
   return S3.upload(s3params, (err, data) => {
     console.log(err, data);
+    console.log(+new Date() - start);
 
-    return S3.getObject(s3GetParams, (err, data) => {
-      console.log('get it!');
+    return S3.listObjectsV2(s3bucket, (err, data) => {
+      console.log('list it!');
       console.log(err, data);
-      console.log(data.Body.toString());
+      console.log(+new Date() - start);
+
+      return S3.getObject(s3GetParams, (err, data) => {
+        console.log('get it!');
+        console.log(err, data);
+        console.log(+new Date() - start);
+        // console.log(data.Body.toString());
+
+        dynamoDb.scan(params, (error, data) => {
+          if (error) {
+            console.log(error);
+          }
+          console.log(error, data.Items);
+          console.log(+new Date() - start);
+        });
+      })
     })
   })
 
-  // return dynamoDb.scan(params, (error, data) => {
-  //   if (error) {
-  //     callback(error);
-  //   }
-  //   callback(error, data.Items);
-  // });
 };
